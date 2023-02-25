b2f6ca16a1d23abec38839ae14beb5434c4056d3
Loay Halawani <81193505+loayhalawani@users.noreply.github.com>
2023-02-26 00:01:38 +0200

    Add files via upload

---
+++ static.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+
+int main(int argc, char* argv[]) {
+  if(argc != 8) {
+    printf("Use this command to display the end result as an image: ./outputFile <xmin> <xmax> <ymin> <ymax> <maxiter> <xres> <outputImage.ppm>\n", argv[0]);
+    printf("Example: ./outputFile 0.27085 0.27100 0.004640 0.004810 1000 1024 image.ppm\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  const double xmin = atof(argv[1]);
+  const double xmax = atof(argv[2]);
+  const double ymin = atof(argv[3]);
+  const double ymax = atof(argv[4]);
+  const uint16_t maxiter = (unsigned short) atoi(argv[5]);
+  const int xres = atoi(argv[6]);
+  const int yres = (xres * (ymax - ymin)) / (xmax - xmin);
+  const char* filename = argv[7];
+
+  FILE *fp = fopen(filename,"wb");
+  char *comment = "# Mandelbrot set";
+
+  fprintf(fp, "P6\n# Mandelbrot, xmin=%lf, xmax=%lf, ymin=%lf, ymax=%lf, maxiter=%d\n%d\n%d\n%d\n", xmin, xmax, ymin, ymax, maxiter, xres, yres, (maxiter < 256 ? 256 : maxiter));
+
+  double dx = (xmax - xmin) / xres;
+  double dy = (ymax - ymin) / yres;
+  double x, y;
+  double u, v;
+  int i,j;
+  int k;
+
+  for(j = 0; j < yres; j ++) {
+    y = ymax - j * dy;
+    for(i = 0; i < xres; i ++) {
+      double u = 0.0;
+      double v = 0.0;
+      double u2 = u * u;
+      double v2 = v * v;
+      x = xmin + i * dx;
+      for(k = 1; k < maxiter && (u2 + v2 < 4.0); k ++) {
+            v = 2 * u * v + y;
+            u = u2 - v2 + x;
+            u2 = u * u;
+            v2 = v * v;
+      };
+      if (k >= maxiter) {
+        const unsigned char black[] = {0, 0, 0, 0, 0, 0};
+        fwrite(black, 6, 1, fp);
+      }
+      else {
+        unsigned char color[6];
+        color[0] = k >> 8;
+        color[1] = k & 255;
+        color[2] = k >> 8;
+        color[3] = k & 255;
+        color[4] = k >> 8;
+        color[5] = k & 255;
+        fwrite(color, 6, 1, fp);
+      };
+    }
+  }
+
+  fclose(fp);
+  return 0;
+}
+
